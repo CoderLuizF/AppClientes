c9a916b691e9df277c39bb2054abda2cfa949953
Luiz Felipe <zzzezizzz@gmail.com>
2026-05-04 11:39:20 -0300

    feat: add ClientRepository with CRUD display methods

--- ClientRepository.cs
+++ ClientRepository.cs
@@ -1,6 +1,31 @@
+using Cadastro;
+
 namespace Repository;
 
 public class ClientRepository
 {
-    
+    public List<Cliente> clients = new List<Cliente>();
+
+    public void PrintClient(Cliente client)
+    {
+        Console.WriteLine("ID.............:" + client.Id);
+        Console.WriteLine("Name.............:" + client.Name);
+        Console.WriteLine("Discount.............:" + client.Discount.ToString("0.00"));
+        Console.WriteLine("Birth Date.............:" + client.BirthDate);
+        Console.WriteLine("Registration Date.............:" + client.RegistrationDate);
+        Console.WriteLine("---------------------------------------------------");
+    }
+
+    public void ShowClients()
+    {
+        Console.Clear();
+        
+        foreach(var client in clients)
+        {
+            PrintClient(client);
+        }
+        
+        Console.ReadKey();
+    }
+
 }
