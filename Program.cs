1c53a42bf252dae83ebb9501f5014161a0720e14
Luiz Felipe <zzzezizzz@gmail.com>
2026-05-04 11:09:15 -0300

    feat: add main menu UI

--- Program.cs
+++ Program.cs
@@ -6,4 +6,16 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    static void Menu()
+{
+    Console.WriteLine("Customer Registration");
+    Console.WriteLine("--------------------");
+    Console.WriteLine("1- Register Customer");
+    Console.WriteLine("2- List Customers");
+    Console.WriteLine("3- Edit Customer");
+    Console.WriteLine("4- Delete Customer");
+    Console.WriteLine("5- Exit");
+    Console.WriteLine("--------------------");
+}
 }
